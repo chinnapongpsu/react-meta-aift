fac814d87e07b4f271b8bec4fedbdb3226e2c3b8
NakLast <nackwoung@gmail.com>
2024-11-10 08:54:46 +0700

    Edit text dialog.

--- backend-fast/main.py
+++ backend-fast/main.py
@@ -17,12 +17,11 @@ app.add_middleware(
 
 
 @app.post("/chat")
-async def chat(prompt: str = Body(...),
-               sessionid: str= Body(...)):
-    context_file_path = 'context_text.txt'
+async def chat(prompt: str = Body(...), sessionid: str = Body(...)):
+    context_file_path = "context_text.txt"
 
     try:
-        with open(context_file_path, 'r', encoding='utf-8') as file:
+        with open(context_file_path, "r", encoding="utf-8") as file:
             context_text = file.read()
     except FileNotFoundError:
         return {"error": "context.txt not found"}
@@ -30,18 +29,14 @@ async def chat(prompt: str = Body(...),
     print(context_text)
     url = "https://api.aiforthai.in.th/pathumma-chat"
     payload = {
-        'prompt': prompt,
-        'sessionid': sessionid, 
-        'context': context_text,
-        'temperature':0.4
+        "prompt": prompt,
+        "sessionid": sessionid,
+        "context": context_text,
+        "temperature": 0.4,
     }
 
-    headers = {
-        'apikey': 'JY3kRvMpaatLsl4CEvfxS21Z4bAuk3Jm'
-
-    }
+    headers = {"apikey": "JY3kRvMpaatLsl4CEvfxS21Z4bAuk3Jm"}
 
     response = requests.post(url, headers=headers, data=payload)
     response_json = json.loads(response.text)
-    return {"response":response_json['response']}
-
+    return {"response": response_json["response"]}
